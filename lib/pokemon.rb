bf7658cf65623c1981a629725f472d47b41f46b8
Oana Berentan <oana.berentan@yahoo.com>
2019-09-24 19:43:41 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -9,10 +9,11 @@ class Pokemon
  end
 
   def self.save(name, type, db)
-    db.execute("INSERT INTO pokemon (name, type) VALUES (?,?)", name, type)
+    DB[:conn].execute("INSERT INTO pokemon (name, type) VALUES (?,?)", name, type)
   end
 
- def self.find
+ def self.find(id)
+   db.
  end
 
 end #class
